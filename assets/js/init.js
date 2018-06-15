d3c91faf025d29b2d61a5fad18bc9f411d6b1d4d
Mateusz Byczek <mateusz.byczek@moto-profil.pl>
2018-06-15 13:37:18 +0200

    - Saving token in coockie, and veryfing if token is in coockie. - Added dashboard TODO: - verify if token is valid - generate something in dashboard ;)

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,16 +1,53 @@
 $(document).ready(function(){
-    setTimeout(function(){
-        $('#loader').animate({
-            opacity: 0,
-        },400, function(){
-            $('#loader').addClass('hidden');
-            $('#login-page').css("opacity","0");
-            setTimeout(function(){
-                $('#login-page').removeClass('hidden');
-                $('#login-page').animate({
-                    opacity: 1,
-                },400, function(){});
-            },100)
-        });
-    },1000);
+    if(getAuthCookie() === "")
+    {
+        setTimeout(function(){
+            $('#loader').animate({
+                opacity: 0,
+            },400, function(){
+                $('#loader').addClass('hidden');
+                $('#login-page').css("opacity","0");
+                setTimeout(function(){
+                    $('#login-page').removeClass('hidden');
+                    $('#login-page').animate({
+                        opacity: 1,
+                    },400, function(){});
+                },100)
+            });
+        },1000);
+    }
+    else
+    {
+        setTimeout(function(){
+            $('#loader').animate({
+                opacity: 0,
+            },400, function(){
+                $('#loader').addClass('hidden');
+                $('#dashboard').css("opacity","0");
+                setTimeout(function(){
+                    $('#dashboard').removeClass('hidden');
+                    $('#dashboard').animate({
+                        opacity: 1,
+                    },400, function(){});
+                },100)
+            });
+        },100);
+    }
 });
+
+function setAuthCookie(token){
+    document.cookie = "Authorization=" + 'bearer '+token;
+}
+
+function getAuthCookie(){
+    var cn = "Authorization=";
+    var idx = document.cookie.indexOf(cn)
+ 
+    if (idx != -1) {
+        var end = document.cookie.indexOf(";", idx + 1);
+        if (end == -1) end = document.cookie.length;
+        return unescape(document.cookie.substring(idx + cn.length, end));
+    } else {
+        return "";
+   }
+ }
--- assets/js/loginPage.js
+++ assets/js/loginPage.js
@@ -13,8 +13,21 @@
                 $('.login-form button').addClass('loading');
             },
             success: function(data){
+                setAuthCookie(data)
                 $('.login-form button').text('Success!');
                 $('.login-form button').removeClass('btn-error').addClass('btn-success').removeClass('loading');
+                setTimeout(() => {
+                    $('#dashboard').css("opacity","0");
+                    $('#login-page').animate({
+                        opacity: 0,
+                    },400, function(){
+                        $('#login-page').addClass('hidden');
+                        $('#dashboard').removeClass('hidden');
+                        $('#dashboard').animate({
+                            opacity: 1,
+                        },400, function(){})
+                    });
+                }, 500);
             },
             error: function(data){
                 $('.login-form button').text('Error. Please try again...');
